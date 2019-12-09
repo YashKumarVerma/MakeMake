99884e02ff0d2adc37289485244c256fa2a31a58
Yash Kumar Verma <yk.verma2000@gmail.com>
2019-12-09 13:44:02 +0530

    update

--- source/app.cpp
+++ source/app.cpp
@@ -3,12 +3,33 @@
 @desc:CPP-Template Makefile generator
 */
 #include<iostream>
+#include<filesystem>
+#include<vector>
+#include"./../includes/rainbow/rainbow.h"
 
 using namespace std;
+namespace fs = std::filesystem;
 
-int main(){
+int main(int argc, char** argv){
 
-    
-return 0;
+    // count the number of header files included
+    int headerCounter = 0;
+    for(const auto &n : fs::directory_iterator("./includes")){
+        headerCounter++;
+    }
 
+    // array to store number of header files
+    string *headerFileNames;
+
+    // allocate memory 
+    headerFileNames = (string*)malloc(headerCounter*sizeof(string));
+
+    // loop through the data and append to array
+    for (const auto & entry : fs::directory_iterator("./includes")){
+        string fileName = entry.path();
+        fileName = fileName.substr(11, fileName.length());
+        cout << fileName << endl;
+    }
+
+    return 0;
 }

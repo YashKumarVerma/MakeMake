a77515a2711e2014e7281fa0b38d7f876a98a7f3
Yash Kumar Verma <yk.verma2000@gmail.com>
2019-12-17 00:22:34 +0530

    basic makefile generator

--- source/app.cpp
+++ source/app.cpp
@@ -4,32 +4,58 @@
 */
 #include<iostream>
 #include<filesystem>
+#include<fstream>
 #include<vector>
-#include"./../includes/rainbow/rainbow.h"
 
 using namespace std;
 namespace fs = std::filesystem;
 
 int main(int argc, char** argv){
+    // to store moduleNames
+    vector<string> headerNames;
 
-    // count the number of header files included
-    int headerCounter = 0;
-    for(const auto &n : fs::directory_iterator("./includes")){
-        headerCounter++;
-    }
-
-    // array to store number of header files
-    string *headerFileNames;
-
-    // allocate memory 
-    headerFileNames = (string*)malloc(headerCounter*sizeof(string));
-
+    // open write stream to write makefile
+    ofstream makeFile;
+    makeFile.open("Makefile");
+    
     // loop through the data and append to array
     for (const auto & entry : fs::directory_iterator("./includes")){
+        // get filename
         string fileName = entry.path();
         fileName = fileName.substr(11, fileName.length());
-        cout << fileName << endl;
+        
+        // push data into headerNames vector
+        headerNames.push_back(fileName);
     }
 
+    // write the header to makefile
+    makeFile << "bin/makemake: bin/app.o ";
+    for(int i=0; i<headerNames.size(); i++){
+
+        // link all the object files of modules
+        makeFile << " bin/" << headerNames[i] << ".o ";
+    }
+
+    // to write the #2, the cmd which runs if #1 is changed
+    makeFile << "\n" << "\tg++ --std=c++17 bin/app.o ";
+    for(int i=0; i<headerNames.size(); i++){
+        makeFile << "bin/" << headerNames[i] << ".o "; 
+    }
+    makeFile << " -o bin/makemake";
+
+    // the rule which directly works on app.cpp -> app.o
+    makeFile << "\n\nbin/app.o: source/app.cpp\n\tg++ --std=c++17 -c source/app.cpp -o bin/app.o";
+
+    // rule to check for changes in modules itself
+    for(int i=0; i<headerNames.size(); i++){
+        makeFile << "\n\nbin/" << headerNames[i] << ".o: includes/" << headerNames[i] << "/" << headerNames[i] << ".cpp includes/" << headerNames[i] << "/" << headerNames[i] << ".h\n";
+        makeFile << "\tg++ --std=c++17 -c includes/"<< headerNames[i] << "/"<< headerNames[i] << ".cpp -o bin/"<< headerNames[i] <<".o"; 
+    }
+
+    // code the clean function
+    makeFile << "\n\n" << "clean:\n\trm -rf bin/*.o\n";
+
+    // close the file
+    makeFile.close();
     return 0;
 }
